82464da713a7cf7deb076c55e6cc70f93877c944
YukiNyaa <yukinyaa712@gmail.com>
2016-11-08 21:20:53 +0900

    initial commit

---
+++ Assets/studyScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class studyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			float rnd = Random.Range(0.0f,0.5f);
+			this.transform.position = new Vector3(0.0f,0.0f,rnd);
+		}
+		if (Input.GetKeyDown (KeyCode.B)) 
+		{
+			float rnd = Random.Range(0.0f,360.0f);
+			this.transform.rotation = Quaternion.Euler(rnd,0.0f,0.0f);
+		}
+	}
+}
